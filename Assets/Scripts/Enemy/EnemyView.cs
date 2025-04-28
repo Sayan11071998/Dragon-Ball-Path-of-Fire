475d04754215e43f3e90354a93d9e19165a9964f
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-29 02:12:33 +0530

    Kick Animation Working

--- Assets/Scripts/Enemy/EnemyView.cs
+++ Assets/Scripts/Enemy/EnemyView.cs
@@ -1,15 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 namespace DragonBall.Enemy
 {
     public class EnemyView : MonoBehaviour, IDamageable
     {
+        [SerializeField] private Collider2D attackHitBox;
+        [SerializeField] private float attackDuration = 2f;
+
         private EnemyController controller;
         private Rigidbody2D rb;
         private Animator animator;
         private SpriteRenderer spriteRenderer;
 
         private bool isMoving = false;
+        private bool isAttacking = false;
+
+        public bool IsAttacking => isAttacking;
 
         public void SetController(EnemyController controllerToSet) => controller = controllerToSet;
 
@@ -18,6 +25,9 @@ namespace DragonBall.Enemy
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
             spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (attackHitBox != null)
+                attackHitBox.enabled = false;
         }
 
         private void FixedUpdate() => controller?.FixedUpdate();
@@ -54,12 +64,37 @@ namespace DragonBall.Enemy
                 spriteRenderer.flipX = directionX < 0;
         }
 
+        public void StartAttack()
+        {
+            if (isAttacking) return;
+            isAttacking = true;
+            animator.SetBool("isAttacking", true);
+            StartCoroutine(AttackCoroutine());
+        }
+
+        private IEnumerator AttackCoroutine()
+        {
+            attackHitBox.enabled = true;
+            yield return new WaitForSeconds(attackDuration);
+            attackHitBox.enabled = false;
+            isAttacking = false;
+            animator.SetBool("isAttacking", false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!isAttacking) return;
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player got damaged");
+                // later: other.GetComponent<PlayerController>().TakeDamage(enemySO.AttackDamage);
+            }
+        }
+
         private void UpdateAnimator()
         {
             if (animator != null)
-            {
                 animator.SetBool("isMoving", isMoving);
-            }
         }
     }
 }
