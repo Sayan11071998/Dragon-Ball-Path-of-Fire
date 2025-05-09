61446709dc3fa61d8340d231e0669ec1e243ca32
Sayan Nandi <sayannandi11071998@gmail.com>
2025-05-09 13:49:00 +0530

    Scene Loading Added

---
+++ Assets/Scripts/Level/LevelCompleteTrigger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using DragonBall.Core;
+
+namespace DragonBall.Level
+{
+    public class LevelCompleteTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            GameService.Instance.playerService.PlayerController.DisablePlayerController();
+            StartCoroutine(LoadNextSceneAfterDelay(2f));
+        }
+
+        private IEnumerator LoadNextSceneAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
